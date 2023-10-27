ca50c308087bd826e55999ebd0679fb1ade5dd10
yoshida0835 <141699753+yoshida0835@users.noreply.github.com>
2023-10-28 00:31:55 +0900

    ユーザー新規登録・ログイン画面・トップページ修正

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  
+  # ユーザー新規登録についてのテストコード
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+    it 'emailが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します
+    end
+    it 'passwordが空では登録できない' do
+      # passwordが空では登録できないテストコードを記述します
+    end
+    it 'family_nameが空では登録できない' do
+      # family_nameが空では登録できないテストコードを記述します
+    end
+    it 'first_nameが空では登録できない' do
+      # first_nameが空では登録できないテストコードを記述します
+    end
+    it 'family_name_kanaが空では登録できない' do
+      # family_name_kanaが空では登録できないテストコードを記述します
+    end
+    it 'first_name_kanaが空では登録できない' do
+      # first_name_kanaが空では登録できないテストコードを記述します
+    end
+    it 'birthdayが空では登録できない' do
+      # birthdayが空では登録できないテストコードを記述します
+    end
+  end
+
+end
