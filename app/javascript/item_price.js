25ecd7b06f41aaa6a2635ae0e40536773e7b0368
yoshida0835 <141699753+yoshida0835@users.noreply.github.com>
2023-11-01 14:38:02 +0900

    javascript修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,7 +4,7 @@ function tax (){
     const taxPrice = Math.floor(itemPrice.value * 0.1);
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${taxPrice}`;
-    const salesProfit = itemPrice.value - taxPrice;
+    const salesProfit = Math.floor(itemPrice.value - taxPrice);
     const profitPrice = document.getElementById("profit");
     profitPrice.innerHTML = `${salesProfit}`;
   });
