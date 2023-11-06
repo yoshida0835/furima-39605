fb0cec7d3a01207a1bf22671f64779c29c779daf
yoshida0835 <141699753+yoshida0835@users.noreply.github.com>
2023-11-06 14:45:40 +0900

    beroreaction作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,16 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -43,9 +41,8 @@ class ItemsController < ApplicationController
                                  :price).merge(user_id: current_user.id)
   end
 
-  def item_params
-    params.require(:item).permit(:name, :image, :detail, :category_id, :status_id, :delivery_cost_id, :prefecture_id, :delivery_day_id,
-      :price)
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
