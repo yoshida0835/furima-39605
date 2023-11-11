1d1dee763e001ccebed3977a46c01dbe01452e52
yoshida0835 <141699753+yoshida0835@users.noreply.github.com>
2023-11-11 22:48:17 +0900

    カード情報以外のモデルテスト

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,13 +6,12 @@ class OrderAddress
     validates :user_id, :item_id, :city, :street
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :tel_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :tel_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Use only digits without hyphen' }
   end
-  validates :building
 
   def save
-    order = Order.create(user_id:, item_id:)
-    Address.create(post_code:, prefecture_id:, city:, street:, building:,
-                   tel_number:, order_id: order.id)
+    order = Order.create(user_id: use_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, street: street, building: building,
+                   tel_number: tel_number, order_id: order_id)
   end
 end
