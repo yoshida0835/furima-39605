683864f6386752d3dde5ac16ddc533205253f739
yoshida0835 <141699753+yoshida0835@users.noreply.github.com>
2023-11-17 23:45:29 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items do
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+  end
+    resources :items do
     resources :orders, only: [:index, :create]
     resource  :likes,  only: [:create, :destroy]
   end
+
 end
