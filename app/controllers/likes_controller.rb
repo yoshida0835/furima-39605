e527cc09dea684957317c989011b4b304a9b3440
yoshida0835 <141699753+yoshida0835@users.noreply.github.com>
2023-11-17 21:39:26 +0900

    topページのハート実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,22 +1,6 @@
 class LikesController < ApplicationController
 before_action :like_item
 
-#  def create
-#    like = current_user.likes.build(item_id: params[:item_id])
-#    like.save
-#    respond_to do |format|
-#      format.js
-#    end
-#  end
-
-#def destroy
-#  like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
-#  like.destroy
-#  respond_to do |format|
-#    format.js
-#  end
-#end
-
   def create
     like = current_user.likes.build(item_id: params[:item_id])
     like.save
