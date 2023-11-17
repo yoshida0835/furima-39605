4d0d93bc7ff4950ba87496e8f7028a380a0d6dd0
yoshida0835 <141699753+yoshida0835@users.noreply.github.com>
2023-11-17 12:42:55 +0900

    いいね済か判定

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,4 +19,8 @@ class Item < ApplicationRecord
 
   validates :category_id, :status_id, :delivery_cost_id, :prefecture_id, :delivery_day_id,
             numericality: { other_than: 1, message: "can't be blank" }
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
