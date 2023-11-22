f3d9110f6ae27d76965d81b04b3cccdd7c10b0dd
yoshida0835 <141699753+yoshida0835@users.noreply.github.com>
2023-11-22 23:17:37 +0900

    ActionCable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
+    @item = Item.find(params[:item_id])
     if @comment.save
-      redirect_to item_path(params[:item_id])
+      CommentChannel.broadcast_to @item, { comment: @comment, user: @comment.user }
     end
   end
 
